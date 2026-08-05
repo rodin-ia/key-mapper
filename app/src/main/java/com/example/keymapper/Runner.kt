dd8dca938ca2a2df3ae8207e5b4a2bf509137a6b
rodin-ia <38556724+rodin-ia@users.noreply.github.com>
2026-08-05 22:13:00 +0300

    Update Runner.kt

--- app/src/main/java/com/example/keymapper/Runner.kt
+++ app/src/main/java/com/example/keymapper/Runner.kt
@@ -13,6 +13,37 @@ class Runner(private val context: Context) {
         intent.setPackage(packageName)
         context.startActivity(intent)
     }
+    fun runPackage(packageName: String) {
+
+        val intent = context.packageManager
+        .getLaunchIntentForPackage(packageName)
+
+        if (intent != null) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+        }
+    }
+
+    fun runSettings() {
+        val intent = Intent(Settings.ACTION_SETTINGS)
+        context.startActivity(intent)
+    }
+}
+package com.example.keymapper
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+
+class Runner(private val context: Context) {
+    fun run(packageName: String, link: String) {
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setPackage(packageName)
+        context.startActivity(intent)
+    }
 
     fun runSettings() {
         val intent = Intent(Settings.ACTION_SETTINGS)
